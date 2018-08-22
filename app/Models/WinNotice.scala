8652eda03338d423a0ac2ff3533610c1ba86a613
QooChaso <qoochaso238@gmail.com>
2018-08-22 17:45:33 +0900

    動作は確認

--- app/Models/WinNotice.scala
+++ app/Models/WinNotice.scala
@@ -1,9 +1,21 @@
 package Models
 
-case class WinNotice(requestId: String, price: Float)
+import play.api.libs.json.{Json, Writes}
+import play.api.libs.ws.JsonBodyWritables._
+
+
+case class WinNotice(request_id: String, price: Float)
 
 object WinNotice{
   def apply(request_id: String, price: Float): WinNotice ={
+    new WinNotice(request_id, price)
+  }
 
+  implicit lazy val jsonWrites = new Writes[WinNotice] {
+    def writes(d: WinNotice) =
+      Json.obj(
+        "request_id" -> d.request_id,
+        "price" -> d.price,
+      )
   }
 }
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,39 +5,47 @@ import javax.inject._
 import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.{WSClient, WSRequest}
 import javax.inject.Inject
+import play.api.libs.ws.JsonBodyWritables._
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import play.api.mvc._
 import akka.actor.ActorSystem
 
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import java.io.{FileOutputStream => FileStream, OutputStreamWriter => StreamWriter}
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
-  val DSPurl: String = "http://dsp1.example.jp/req"
-  val DSPurl2: String = "http://dsp1.example.jp/req"
+  val DSPurl: String = "http://localhost:8080/"
   val request1: WSRequest = ws.url(DSPurl)
+  val DSPurl2: String = "http://localhost:8081"
   val request2: WSRequest = ws.url(DSPurl2)
 
   //curl http://localhost:9000/req -X POST -H "Content-Type: application/json" -d '{"app_id": 123}'
+
   //SDKからリクエストを受け取る
   def index: Action[JsValue] = Action(parse.json).async { request =>
     Future {
       (request.body \ "app_id").asOpt[Int] match {
         case Some(s) =>
-          val hoge = List(requestToDsp(s, request1), requestToDsp(s, request1))
-          val futureOfList = Future.sequence(hoge).map{ ddd =>
-            val winD = ddd.maxBy(_.price)
+          //DSPの台数が増えた場合にはここに追加する
+          val responseList: List[Future[dspResponse]] = List(requestToDsp(s, request1), requestToDsp(s, request2))
+          val futureOfList: Future[List[dspResponse]] = Future.sequence(responseList)
+
+          val aa: Future[String] = futureOfList.map{ x =>
+            val winD: dspResponse = x.maxBy(_.price)
             //二番目に高い価格は最大価格のデータを取り除いた要素の最大価格
-            val winPrice = ddd.filterNot(p => p.price == winD.price ).maxBy(_.price).price
-            val winNotice = makeWinNotice(winD, winPrice)
+            //DSPが1つの時の処理も書き加える必要がある
+            val winPrice = x.filterNot(p => p.price == winD.price ).maxBy(_.price).price
+            val winUrl: String = winD.url
+            val winNotice = WinNotice(winD.request_id, winPrice)
+            sendWinNotice(winNotice, ws.url(winD.url))
+            winUrl
           }
-
-          //ここでWinNoticeをPOSTで送る
-
-
-          //ここでSDKにurlのみを返す
-          Ok("")
+          val aaa = Await.result(aa, Duration.Inf)
+          //val sss: Option[Try[String]] = aa.value
+          Ok(Json.toJson("url" -> aaa))
         case None => BadRequest("argment error")
       }
     }
@@ -62,22 +70,26 @@ class HomeController @Inject()(cc: ControllerComponents, ws: WSClient) extends A
     complexRequest
   }
 
-  def makeWinNotice(win: dspResponse, secondPrice: Float): WinNotice ={
-    new
-
-  }
-
   //勝ったDSPにDataを送る
   //この中でlog生成
   def sendWinNotice(postValue: WinNotice, request: WSRequest): Unit ={
     val complexRequest =
       request.addHttpHeaders ("Accept" -> "application/json")
         .withRequestTimeout(100.millis)
-        .post (postValue)
+        .post (Json.toJson(postValue))
+    writeLog(postValue)
   }
 
   //ログファイルに書き込み
   //WinNoticeのmodelを受け取ってパースすればええやろ
-  def writeLog(): Unit ={}
-
+  def writeLog(value: WinNotice): Unit ={
+    val fileName = "log1.txt"
+    val encode = "UTF-8"
+    val append = true
+    // 書き込み処理
+    val fileOutPutStream = new FileStream(fileName, append)
+    val writer = new StreamWriter( fileOutPutStream, encode )
+    writer.write("reqest_id : "+value.request_id+"-----price : "+value.price+"\n")
+    writer.close
+  }
 }
