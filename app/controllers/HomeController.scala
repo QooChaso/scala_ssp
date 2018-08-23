3a92da39df41e1483072a410c44e30ef62c82f66
QooChaso <qoochaso238@gmail.com>
2018-08-23 19:40:10 +0900

    本番環境動作確認

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,17 +5,19 @@ import javax.inject._
 import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.{WSClient, WSRequest}
 import javax.inject.Inject
-import scala.concurrent.{Await, Future}
+
+import scala.concurrent.{Await, Future, TimeoutException}
 import play.api.mvc._
+
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
-  val DSPurl: String = "http://localhost:8080/"
-  val request1: WSRequest = ws.url(DSPurl)
+  val DSPurl: String = "http://localhost:8080"
+  val request1: WSRequest = ws.url(DSPurl+"/req")
   val DSPurl2: String = "http://localhost:8081"
-  val request2: WSRequest = ws.url(DSPurl2)
+  val request2: WSRequest = ws.url(DSPurl2+"/req")
 
   //curl http://localhost:9000/req -X POST -H "Content-Type: application/json" -d '{"app_id": 123}'
 
@@ -25,20 +27,22 @@ class HomeController @Inject()(cc: ControllerComponents, ws: WSClient) extends A
       (request.body \ "app_id").asOpt[Int] match {
         case Some(s) =>
           //DSPの台数が増えた場合にはここに追加する
-          val responseList: List[Future[dspResponse]] = List(DspManager.requestToDsp(s, request1), DspManager.requestToDsp(s, request2))
-          val futureOfList: Future[List[dspResponse]] = Future.sequence(responseList)
-
-          val winReqestProcess: Future[String] = futureOfList.map{ x =>
+          val responseList: List[Future[Option[dspResponse]]] =
+            List(DspManager.requestToDsp(s, request1, DSPurl), DspManager.requestToDsp(s, request2, DSPurl2))
+          val futureOfList: Future[List[Option[dspResponse]]] = Future.sequence(responseList)
+          val hoge: Future[List[dspResponse]] = futureOfList.map{ x => x.flatten }
+          val winReqestProcess: Future[String] = hoge.map{ x =>
+            var winPrice: Float = 0
             val winD: dspResponse = x.maxBy(_.price)
-            //二番目に高い価格は最大価格のデータを取り除いた要素の最大価格
-            val winPrice = x.filterNot(p => p.price == winD.price ).maxBy(_.price).price + 1
+            //セカンドプライス = 最大価格のデータを取り除いた要素の最大価格 + 1
+            if(x.length >= 2) winPrice = x.filterNot(p => p.price == winD.price ).maxBy(_.price).price + 1
+            else winPrice += 1
             val winUrl: String = winD.url
             val winNotice = WinNotice(winD.request_id, winPrice)
-            DspManager.sendWinNotice(winNotice, ws.url(winD.url))
+            DspManager.sendWinNotice(winNotice, ws.url(winD.reqUrl))
             winUrl
           }
           val winUrl: String = Await.result(winReqestProcess, Duration.Inf)
-          //val sss: Option[Try[String]] = aa.value
           Ok(Json.toJson("url" -> winUrl))
         case None => BadRequest("argment error")
       }
--- app/models/dspResponse.scala
+++ app/models/dspResponse.scala
@@ -1,9 +1,9 @@
 package models
 
-case class dspResponse(request_id: String, url: String, price: Float)
+case class dspResponse(request_id: String, url: String, price: Float, reqUrl: String)
 
 object dspResponse{
-  def apply(request_id: String, url: String, price: Float): dspResponse = {
-    new dspResponse(request_id, url, price)
+  def apply(request_id: String, url: String, price: Float, url2: String): dspResponse = {
+    new dspResponse(request_id, url, price, url2)
   }
 }
